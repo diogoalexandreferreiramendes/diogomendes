4c5f3195eef5b6ea62cdcd2f8ccd5be0da79bbca
Diogo Mendes <diogoafm6@gmail.com>
2022-01-26 12:41:48 +0000

    https 4º

--- app.js
+++ app.js
@@ -12,13 +12,15 @@ app.use('/',express.static(path.join(__dirname, 'build')));
 app.use('/*',(req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
       if (req.headers.host === 'diogomendes.net/')
-          return res.redirect(301, 'https://www.diogomendes.net');
+          res.redirect(301, 'https://www.diogomendes.net');
+          express.static(path.join(__dirname, 'build'))
       if (req.headers['x-forwarded-proto'] !== 'https')
           return res.redirect('https://' + req.headers.host + req.url);
       else
           return next();
-  } else
-    express.static(path.join(__dirname, 'build'))
+  } else{
+    return next();
+  }
 });
 
 app.get('/*', function (req, res) {
