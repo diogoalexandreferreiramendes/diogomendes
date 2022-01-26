485d2d7f6dcdef52799878ab43a80d3de9fdb777
Diogo Mendes <diogoafm6@gmail.com>
2022-01-26 17:04:13 +0000

    https 9

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const app = express();
 
 const port = process.env.PORT || 9000;
 
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-      if (req.headers.host === 'thediogomendes.herokuapp.com')
-          return res.redirect(301, 'https://www.diogomendes.net');
-      if (req.headers['x-forwarded-proto'] !== 'https')
-          return res.redirect('https://' + req.headers.host + req.url);
-    }else
-      return next();
-});
+// app.use((req, res, next) => {
+//   if (process.env.NODE_ENV === 'production') {
+//       if (req.headers.host === 'thediogomendes.herokuapp.com')
+//           return res.redirect(301, 'https://www.diogomendes.net');
+//       if (req.headers['x-forwarded-proto'] !== 'https')
+//           return res.redirect('https://' + req.headers.host + req.url);
+//     }else
+//       return next();
+// });
 
-//app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  //res.send('HELLLO FROM THE OTHER SIDE');
 });
 
 
